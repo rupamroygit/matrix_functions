2aa6d51a2fc85142ad354da11410e3ceb1722d80
Rupam Roy <rupam.contact@gmail.com>
2023-12-16 23:37:59 +0530

    only print & Display Fucntion V-1

---
+++ martrix_add_mul_sub.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#define ROW 2
+#define COL 3
+
+/*To calculate addition, subtraction and multiplication of 2-
+dimensional matrix using function.*/
+
+void display(int arr[ROW][COL])
+{
+    int i,j;
+    for(i=0;i<ROW;i++)
+    {
+        for(j=0;j<COL;j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }
+        printf("\n");
+    }
+}
+void input(int arr[ROW][COL])
+{
+    int i,j;
+    for(i=0;i<ROW;i++)
+    {
+        for(j=0;j<COL;j++)
+        {
+            printf("Enter array %d&%d Element\n: ",i,j);
+            scanf("%i",&arr[i][j]);
+        }
+    }
+}
+void main()
+{
+    int array[ROW][COL];
+}
