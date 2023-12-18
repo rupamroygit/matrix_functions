18835c0d9678c1ecb506d688dd1956f67ded0df3
Rupam Roy <rupam.contact@gmail.com>
2023-12-18 12:02:42 +0530

    v-1.7
    matrix multiply added

--- martrix_add_mul_sub.c
+++ martrix_add_mul_sub.c
@@ -43,16 +43,33 @@ int addtion(int arr1[ROW][COL],int arr2[COL][ROW],int arrsum[ROW][COL])
 }
 void matrix_multiply(int arr1[ROW][COL],int arr2[ROW][COL],int arrmul[ROW][COL])
 {
-    
+    int i,j,k;
+    for(i=0;i<ROW;i++)
+    {
+        for(j=0;j<COL;j++)
+        {
+            for(k=0;k<COL;k++)
+            {
+                arrmul[i][j]+=arr1[i][k]*arr2[k][j];
+            }
+        }
+    }
 }
 void main()
 {
     int array1[ROW][COL],array2[ROW][COL],arrstore[ROW][COL];
     input(array1);
     input(array2);
+    printf("Array No 1:\n");
     display(array1);
+    printf("Array No 2:\n");
     display(array2);
     //arrstore[ROW][COL]=addtion(array1,array2);
     addtion(array1,array2,arrstore);
+    printf("Array Sum:\n");
+    display(arrstore);
+
+    matrix_multiply(array1,array2,arrstore);
+    printf("Array Multiply:\n");
     display(arrstore);
 }
